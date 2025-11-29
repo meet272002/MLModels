572a546b7f8f48610849d2e295eca5598a8adb5f
Meet-Gandhi <meet272002@gmail.com>
2025-11-29 22:49:06 +0530

    Error resolved

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from train_test_split import model_selection as ms
+from sklearn.preprocessing import StandardScaler
 
 # Created By Meet Gandhi on 28/11/2025 | Class Creation for linear regression code.
+# Updated By Meet Gandhi on 29/11/2025 | Major changes and error Solving.
 class LinearRegression:
     def __init__(self,learning_rate = .1,iteration = 1000):
         self.learning_rate = learning_rate
@@ -12,9 +14,9 @@ class LinearRegression:
         self.training_examples,self.features = X.shape[0],X.shape[1]
 
         self.W = np.zeros((self.features,1))
-        self.b = 0
+        self.b = 0.0
         self.X = X
-        self.y = y
+        self.y = y.values.flatten()
         self.m = X.shape[0]
 
         for i in range(self.iteration):
@@ -27,8 +29,9 @@ class LinearRegression:
 
         residual = y_predict - self.y
 
-        gradient_w = (1/self.m)*(self.X.T)@residual
-        gradient_b = np.sum(residual)/self.m
+        residual_col_vector = residual.reshape(-1, 1)
+        gradient_w = (1/self.m)*(self.X.T)@residual_col_vector
+        gradient_b = np.sum(residual_col_vector)/self.m
 
         self.W  = self.W - self.learning_rate*gradient_w
         self.b = self.b - self.learning_rate*gradient_b
@@ -37,18 +40,32 @@ class LinearRegression:
 
     def predict(self,X):
         y_predicted = X @ self.W + self.b
-        return y_predicted
+        return y_predicted.values.flatten()
 
 def main():
     train = pd.read_csv("train.csv")
-    X = train.drop(columns=['is_positive'])
-    y = train[['is_positive']]
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+    numeric_cols = []
+    for col in train.columns:
+        if train[col].dtype != 'object' and col != 'is_positive' and col != 'review_id':
+            numeric_cols.append(col)
 
-    model = LinearRegression()
-    model.fit(X,y)
+    train.dropna(inplace=True)
+    X = train[numeric_cols]
+    y = train['is_positive']
 
-    print(model.predict(X))
+    ss = StandardScaler()
+    X_scaled = pd.DataFrame(ss.fit_transform(X),columns=X.columns,index=X.index)
 
+    X_train, X_test, Y_train, Y_test = ms.train_test_split(ms,X_scaled,y,test_size = .3)
+
+    model = LinearRegression(learning_rate=.00001,iteration=10)
+    model.fit(X_train,Y_train)
+
+    y_predicted = model.predict(X_test)
+
+    error = (y_predicted - Y_test)**2
+
+    final = sum(error)/Y_test.shape[0]
+    print(final)
 main()
